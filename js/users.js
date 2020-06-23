aa87a245e4d421e1a02e7af3c672863ffc24c4e7
mort79f8 <mort79f8@edu.campusvejle.dk>
2020-06-23 12:40:16 +0200

    after lunch commit

--- js/users.js
+++ js/users.js
@@ -7,29 +7,39 @@ fetch(url)
   .then((data) => displayUser(data))
   .catch((err) => console.log(err));
 
+// function to loop through the users
 function displayUser(data) {
   data.forEach((user) => {
     createUserDisplay(user);
   });
 }
 
+// function to create the display used by displayUser() takes a user as an argument.
 function createUserDisplay(user) {
+  //creates the different elements
   const userArticle = document.createElement("article");
   const userImage = document.createElement("img");
   const userName = document.createElement("p");
   const userEmail = document.createElement("p");
   const userPhone = document.createElement("p");
   const userCity = document.createElement("p");
-  const userNameText = document.createTextNode(user.name);
-  const userEmailText = document.createTextNode(user.email);
-  const userPhoneText = document.createTextNode(user.phone);
-  const userCityText = document.createTextNode(user.address.city);
+  const hr = document.createElement("hr");
+
+  // create the content for the different elements
+  const userNameText = "<span>Brugernavn: </span>" + user.name;
+  const userEmailText = "<span>Email: </span>" + user.email;
+  const userPhoneText = "<span>Telefon: </span>" + user.phone;
+  const userCityText = "<span>By: </span>" + user.address.city;
+
+  // add the content to the elements
   userImage.src = "./img/randomUser.svg";
-  userName.appendChild(userNameText);
-  userEmail.appendChild(userEmailText);
-  userPhone.appendChild(userPhoneText);
-  userCity.appendChild(userCityText);
+  userName.innerHTML = userNameText;
+  userEmail.innerHTML = userEmailText;
+  userPhone.innerHTML = userPhoneText;
+  userCity.innerHTML = userCityText;
+
   userArticle.appendChild(userImage);
+  userArticle.appendChild(hr);
   userArticle.appendChild(userName);
   userArticle.appendChild(userEmail);
   userArticle.appendChild(userPhone);
